dfb754bfd2fe9dcc80ee46d8fce9323bbdbcbd27
Olga <olgasoboleva99@gmail.com>
2019-11-08 15:39:17 +0300

    cors

---
+++ src/main/java/gateway/security/CorsFilterConfiguration.java
@@ -0,0 +1,30 @@
+package gateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsFilterConfiguration {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        return new CorsWebFilter(corsConfigurationSource());
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedMethod(HttpMethod.POST);
+        config.addAllowedMethod(HttpMethod.GET);
+        config.addExposedHeader("Authorization");
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
